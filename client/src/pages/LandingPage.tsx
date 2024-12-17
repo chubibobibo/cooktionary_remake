e45164d5e1b574a86125e8e1d2cc4d672c984d80
Alistair Abao <abaolester@gmail.com>
2024-12-17 17:05:06 +0100

    resposnive layout for bigger screens

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -3,43 +3,51 @@ import { customTheme } from "../utils/themes/customThemes";
 
 function LandingPage() {
   return (
-    <main className={"w-screen h-screen "}>
+    <main className={"w-screen h-screen md:flex flex-col items-center"}>
       <img
         src='../src/assets/landingPage.jpg'
         alt='background image'
         className='w-screen h-screen opacity-15 fixed object-cover'
       />
-      <section className=' h-screen flex items-center flex-col gap-8'>
+      <section className='h-screen flex items-center flex-col gap-8 md:w-11/12 sm:gap-10 md:gap-14'>
         <img
           src='../src/assets/CooktionaryLogo.png'
           alt='cooktionary logo'
-          className=' w-[18rem] h-[18rem] object-cover rounded-full shadow-xl mt-10'
+          className=' w-[18rem] h-[18rem] object-cover rounded-full shadow-xl mt-10 md:w-[30rem] md:h-[30rem] '
         />
-        <section className='flex flex-col gap-2 items-center'>
+        {/* Button container */}
+        <section className='flex flex-col gap-2 items-center sm:gap-4 md:gap-5'>
+          <p className='text-[14px] text-center sm:text-base md:text-lg'>
+            You can create and save you own recipes by logging in
+          </p>
+          <section className='flex flex-col gap-2 md:flex-row'>
+            <Button
+              theme={customTheme}
+              size='sm'
+              color='customLoginBtn'
+              className='w-40'
+            >
+              Register
+            </Button>
+            <Button
+              theme={customTheme}
+              size='sm'
+              color='customLoginBtn'
+              className='w-40'
+            >
+              Login
+            </Button>
+          </section>
+          <p className='text-[14px] text-center mt-2 sm:text-base md:text-lg'>
+            or check out our library of recipes here:
+          </p>
           <Button
             theme={customTheme}
             size='sm'
             color='customLoginBtn'
             className='w-40'
           >
-            Register
-          </Button>
-          <Button
-            theme={customTheme}
-            size='sm'
-            color='customLoginBtn'
-            className='w-40'
-          >
-            Login
-          </Button>
-          <p>or checkout our library of recipes here:</p>
-          <Button
-            theme={customTheme}
-            size='sm'
-            color='customLoginBtn'
-            className='w-40'
-          >
-            Explore Recipes
+            Browse Recipes
           </Button>
         </section>
       </section>
